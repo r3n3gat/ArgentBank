6fdef9fa05428fefe14c97d22321eda5b14654fc
r3n3gat <r3n3gat@hotmail.com>
2023-11-27 12:42:02 +0100

    install redux & redux toolkit / create ./redux/reducers

--- Front/src/index.js
+++ Front/src/index.js
@@ -4,10 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from "react-redux"
+import { configureStore } from "@reduxjs/toolkit" ;
+import rootReducer from "./redux/reducers";
+
+
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: true,
+})
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
