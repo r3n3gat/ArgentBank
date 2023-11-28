c6949271102f72007c65c19238abe695be264cb5
r3n3gat <r3n3gat@hotmail.com>
2023-11-28 18:31:27 +0100

    profile route protected

--- Front/src/App.js
+++ Front/src/App.js
@@ -3,9 +3,26 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Profile from "./pages/Dashboard";
 import Error from "./pages/Error";
+import React from "react";
+import store from "./redux/store";
 
 // Importing routing functions from React Router
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
+
+const ProtectedRoute = () => {
+  const state = store.getState();
+  const isAuthenticated = state.auth.token !== null;
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" />;
+  }
+
+  return <Profile />;
+};
 
 const router = createBrowserRouter([
   {
@@ -18,7 +35,7 @@ const router = createBrowserRouter([
   },
   {
     path: "/Profile",
-    element: <Profile />,
+    element: <ProtectedRoute />,
   },
   {
     path: "/*",
